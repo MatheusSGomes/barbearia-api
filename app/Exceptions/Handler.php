7d537f13734a90cdab94fa3661b0fa17948bf5f6
Matheus Gomes <math.s.gomes@gmail.com>
2022-09-20 22:06:47 -0300

    Adicionada rota protegida com token usando sanctum

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +48,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+        ? response()->json(['message' => "Não autorizado"], 401)
+        : redirect()->guest($exception->redirectTo() ?? route('index'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,18 @@ Route::apiResource('/agenda', AgendamentoController::class);
 Route::post('/login', function (Request $request) {
     $credentials = $request->only(['email', 'password']);
     $user = User::whereEmail($credentials['email'])->first();
+
+    $token = $user->createToken('token');
+
+    // dd($token->plainTextToken );
+    // return response()->json($token->plainTextToken);
+
     $check = Hash::check($credentials['password'], $user->password);
 
     if ($check) {
-        return ["message" => "Login realizado com sucesso"];
+        return response()->json(["message" => "Login realizado com sucesso"], 200);
     } else {
-        return ["message" => "Senha ou e-mail inválido"];
+        return response()->json(["message" => "Senha ou e-mail inválido"], 401);
     }
 });
 
@@ -28,19 +34,26 @@ Route::post('/register', function (Request $request) {
         $user->password = Hash::make($request->password);
         $user->save();
         
-        return [
+        return response()->json([
             "message" => "Usuário cadastrado com sucesso"
-        ];
+        ], 200);
     } catch (Exception $e) {
-        return [
+        return response()->json([
             "message" => "Não foi possível cadastrar o usuário"
-        ];
+        ]);
     }
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/protegida', function () {
+    return response()->json(["message" => "Rota protegida"], 200);
+})->middleware('auth:sanctum');
+
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::get('/criar-usuario', function() {
 //     $user = Auth::user();
